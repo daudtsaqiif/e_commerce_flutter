144d693ba62d2b6ccd0e2202e4362ab2dd99b8b3
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-12-05 14:21:15 +0700

    p

--- lib/pages/ui/profile_page.dart
+++ lib/pages/ui/profile_page.dart
@@ -5,11 +5,85 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget menuItem(String title) {
+      return Container(
+        margin: const EdgeInsets.only(top: 12),
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+        decoration: BoxDecoration(
+          color: backgroundColor4,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Text(
+              title,
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: regular),
+            ),
+            const Spacer(),
+            const Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      );
+    }
+
     Widget content() {
-      return ListView(
-        children: [
-          
-        ],
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          margin: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Account',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: semiBold,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: menuItem('Edit Profile'),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: menuItem('Your Orders'),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: menuItem('Help'),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'General',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: semiBold,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: menuItem('Privacy & Policy'),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: menuItem('Tems of Service'),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: menuItem('Rate App'),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: menuItem('Help Center'),
+              ),
+            ],
+          ),
+        ),
       );
     }
 
@@ -26,8 +100,8 @@ class ProfilePage extends StatelessWidget {
       );
     }
 
-    return Column(
-      children: [header()],
+    return ListView(
+      children: [header(), content()],
     );
   }
 }
