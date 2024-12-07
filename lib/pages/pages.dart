9a5ac78969fa841a624c2c1a958b20fa0e9900de
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-12-07 11:23:08 +0700

    1

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:e_commerce/pages/ui/widget/widget.dart';
 import 'package:e_commerce/shared/shared.dart';
 import 'package:flutter/material.dart';
--- lib/pages/ui/product_page.dart
+++ lib/pages/ui/product_page.dart
@@ -1,13 +1,462 @@
 part of '../pages.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   const ProductPage({super.key});
 
   @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  List<String> suggestionsProduct = [
+    'assets/image_shoes.png',
+    'assets/image_shoes2.png',
+    'assets/image_shoes3.png',
+    'assets/image_shoes4.png',
+    'assets/image_shoes5.png',
+    'assets/image_shoes6.png',
+    'assets/image_shoes7.png',
+    'assets/image_shoes8.png',
+  ];
+
+  int curretIndex = 0;
+
+  List imagesProduct = [
+    'assets/image_shoes.png',
+    'assets/image_shoes2.png',
+    'assets/image_shoes3.png',
+  ];
+
+  @override
   Widget build(BuildContext context) {
+    Widget content() {
+      return Container(
+        margin: const EdgeInsets.only(top: 20),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: backgroundColor1,
+          borderRadius: const BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Shoes Arei V.2.0 - No Limit',
+              style: primaryTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: semiBold,
+              ),
+            ),
+            Text(
+              'Mountain - Hiking',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: regular,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  NumberFormat.currency(
+                          locale: 'id-ID', symbol: 'IDR', decimalDigits: 0)
+                      .format(750000),
+                  style: priceTextStyle.copyWith(
+                      fontSize: 18, fontWeight: semiBold),
+                ),
+                Container(
+                  width: 30,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: backgroundColor5,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Icon(
+                    Icons.favorite,
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Description',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget imperdiet sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi fringilla lorem a egestas interdum. Integer ac lacus quis est viverra ultricies vel vel mauris. Etiam nulla sapien, cursus sed purus a, posuere laoreet turpis. Integer ut est urna. Suspendisse hendrerit orci nec nulla tempor ornare. Donec dignissim tempor leo, non tempor sem suscipit sed. Proin eu dui eget augue porta laoreet non at augue. Integer varius, massa ut fringilla eleifend, dui metus pellentesque elit, eu posuere arcu justo quis mauris.',
+              style: primaryTextStyle.copyWith(
+                fontSize: 14,
+                fontWeight: regular,
+                letterSpacing: 0.5,
+              ),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Color',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+            ),
+            const SizedBox(height: 10),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.pinkAccent,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.brown,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Size',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+            ),
+            const SizedBox(height: 10),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: backgroundColor1,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '39',
+                        style: primaryTextStyle.copyWith(
+                            fontSize: 16, fontWeight: semiBold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: backgroundColor1,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '40',
+                        style: primaryTextStyle.copyWith(
+                            fontSize: 16, fontWeight: semiBold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: backgroundColor1,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '41',
+                        style: primaryTextStyle.copyWith(
+                            fontSize: 16, fontWeight: semiBold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: backgroundColor1,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '42',
+                        style: primaryTextStyle.copyWith(
+                            fontSize: 16, fontWeight: semiBold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: backgroundColor1,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '43',
+                        style: primaryTextStyle.copyWith(
+                            fontSize: 16, fontWeight: semiBold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Suggestions',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: suggestionsProduct.map((e) {
+                  return Container(
+                    margin: const EdgeInsets.only(right: 16),
+                    width: 150,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: backgroundColor6,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 150,
+                          height: 70,
+                          decoration: BoxDecoration(
+                              color: backgroundColor5,
+                              borderRadius: const BorderRadius.vertical(
+                                top: Radius.circular(8),
+                              ),
+                              image: DecorationImage(
+                                  image: AssetImage(e), fit: BoxFit.cover)),
+                        ),
+                        Text(
+                          'Shoes Arei V.2.0 - No Limit',
+                          style: primaryTextStyle.copyWith(
+                            fontSize: 10,
+                            fontWeight: semiBold,
+                            color: priceColor,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget indicatorBar(int index) {
+      return Container(
+        width: curretIndex == index ? 30 : 10,
+        height: 10,
+        margin: const EdgeInsets.symmetric(horizontal: 2),
+        decoration: BoxDecoration(
+            color: curretIndex == index ? backgroundColor1 : Colors.white,
+            borderRadius: curretIndex == index
+                ? BorderRadius.circular(10)
+                : BorderRadius.circular(5),
+            border: Border.all(color: backgroundColor1)),
+      );
+    }
+
+    Widget header() {
+      int index = 0;
+
+      return Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 20, right: 20, left: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(1),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: backgroundColor1,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.chevron_left,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.all(1),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: backgroundColor1,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.shopping_bag_outlined,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          CarouselSlider(
+            items: imagesProduct
+                .map((element) => Image.asset(
+                      element,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover,
+                    ))
+                .toList(),
+            options: CarouselOptions(
+              height: 210,
+              viewportFraction: 1,
+              enableInfiniteScroll: false,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              onPageChanged: (index, reason) {
+                setState(() {
+                  curretIndex = index;
+                });
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  for (var i = 0; i < imagesProduct.length; i++)
+                    indicatorBar(i),
+                ],
+              )
+            ],
+          )
+        ],
+      );
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text('Product Page'),
+      backgroundColor: backgroundColor6,
+      body: ListView(
+        children: [
+          header(),
+          content(),
+        ],
       ),
     );
   }
